3d7db3fafcef38de685b2a4846e547671cca5376
Purushottam Anand <creativepurus@gmail.com>
2023-08-09 21:24:05 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace Excel_Spreadsheet_Integration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWrite = new System.Windows.Forms.Label();
+            this.labelRead = new System.Windows.Forms.Label();
+            this.buttonWrite = new System.Windows.Forms.Button();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonRead = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // labelWrite
+            // 
+            this.labelWrite.AutoSize = true;
+            this.labelWrite.Location = new System.Drawing.Point(27, 111);
+            this.labelWrite.Name = "labelWrite";
+            this.labelWrite.Size = new System.Drawing.Size(38, 16);
+            this.labelWrite.TabIndex = 20;
+            this.labelWrite.Text = "Write";
+            // 
+            // labelRead
+            // 
+            this.labelRead.AutoSize = true;
+            this.labelRead.Location = new System.Drawing.Point(24, 41);
+            this.labelRead.Name = "labelRead";
+            this.labelRead.Size = new System.Drawing.Size(41, 16);
+            this.labelRead.TabIndex = 19;
+            this.labelRead.Text = "Read";
+            // 
+            // buttonWrite
+            // 
+            this.buttonWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonWrite.Location = new System.Drawing.Point(603, 106);
+            this.buttonWrite.Name = "buttonWrite";
+            this.buttonWrite.Size = new System.Drawing.Size(75, 34);
+            this.buttonWrite.TabIndex = 18;
+            this.buttonWrite.Text = "Write";
+            this.buttonWrite.UseVisualStyleBackColor = true;
+            this.buttonWrite.Click += new System.EventHandler(this.buttonWrite_Click);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"});
+            this.comboBox4.Location = new System.Drawing.Point(250, 112);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 24);
+            this.comboBox4.TabIndex = 17;
+            this.comboBox4.Text = "1";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.comboBox3.Location = new System.Drawing.Point(97, 112);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 24);
+            this.comboBox3.TabIndex = 16;
+            this.comboBox3.Text = "B";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(400, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(148, 22);
+            this.textBox2.TabIndex = 15;
+            this.textBox2.Text = "L";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonRead
+            // 
+            this.buttonRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRead.Location = new System.Drawing.Point(603, 33);
+            this.buttonRead.Name = "buttonRead";
+            this.buttonRead.Size = new System.Drawing.Size(75, 32);
+            this.buttonRead.TabIndex = 14;
+            this.buttonRead.Text = "Read";
+            this.buttonRead.UseVisualStyleBackColor = true;
+            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"});
+            this.comboBox2.Location = new System.Drawing.Point(250, 37);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 13;
+            this.comboBox2.Text = "1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H",
+            "I",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "P",
+            "Q",
+            "R",
+            "S",
+            "T",
+            "U",
+            "V",
+            "W",
+            "X",
+            "Y",
+            "Z"});
+            this.comboBox1.Location = new System.Drawing.Point(97, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.Text = "A";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(400, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 22);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "Waiting....";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 178);
+            this.Controls.Add(this.labelWrite);
+            this.Controls.Add(this.labelRead);
+            this.Controls.Add(this.buttonWrite);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonRead);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWrite;
+        private System.Windows.Forms.Label labelRead;
+        private System.Windows.Forms.Button buttonWrite;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonRead;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace Excel_Spreadsheet_Integration
+{
+    public partial class Form1 : Form
+    {
+        private SheetsService sheetsService;
+        public Form1()
+        {
+            InitializeComponent();
+
+            sheetsService = GoogleSheetsAuthentication.GetService();
+        }
+
+        private void buttonRead_Click(object sender, EventArgs e)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string localExcelFilePath = Path.Combine(currentDirectory, "Spreadsheet_Integration.xlsx");
+
+            List<List<object>> excelData = ReadLocalExcelFile(localExcelFilePath);
+
+            if (excelData.Count > 0)
+            {
+                string message = "Excel Data:\n\n";
+                foreach (var rowData in excelData)
+                {
+                    message += string.Join("\t", rowData) + "\n";
+                }
+
+                MessageBox.Show(message, "Local Excel Data");
+            }
+            else
+            {
+                MessageBox.Show("No data found in the local Excel file.", "Local Excel Data");
+            }
+        }
+
+        private void buttonWrite_Click(object sender, EventArgs e)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string localExcelFilePath = Path.Combine(currentDirectory, "Spreadsheet_Integration.xlsx");
+
+            string spreadsheetId = "1mJaB-cHFzEjvJDtat8mvmrHq5ABqpsW4WR8ZNnRAyHE";
+            string range = "Sheet2!A1:Z1000";
+
+            List<List<object>> excelData = ReadLocalExcelFile(localExcelFilePath);
+            WriteToGoogleSpreadsheet(spreadsheetId, range, excelData);
+        }
+
+        private List<List<object>> ReadLocalExcelFile(string excelFilePath)
+        {
+            List<List<object>> excelData = new List<List<object>>();
+
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(excelFilePath);
+            Excel.Worksheet excelWorksheet = excelWorkbook.Sheets[1];
+
+            int rowCount = excelWorksheet.UsedRange.Rows.Count;
+            int colCount = excelWorksheet.UsedRange.Columns.Count;
+
+            for (int row = 1; row <= rowCount; row++)
+            {
+                List<object> rowData = new List<object>();
+                for (int col = 1; col <= colCount; col++)
+                {
+                    Excel.Range cell = excelWorksheet.Cells[row, col];
+                    rowData.Add(cell.Value);
+                }
+                excelData.Add(rowData);
+            }
+
+            // Close and release Excel objects
+            excelWorkbook.Close(false);
+            excelApp.Quit();
+            Marshal.ReleaseComObject(excelWorksheet);
+            Marshal.ReleaseComObject(excelWorkbook);
+            Marshal.ReleaseComObject(excelApp);
+
+            return excelData;
+        }
+
+        private void WriteToGoogleSpreadsheet(string spreadsheetId, string range, List<List<object>> data)
+        {
+            var valueRange = new ValueRange
+            {
+                Values = data.Select(row => row.Select(cell => cell).ToList()).ToList<IList<object>>()
+            };
+
+            var updateRequest = sheetsService.Spreadsheets.Values.Update(valueRange, spreadsheetId, range);
+            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+
+            var updateResponse = updateRequest.Execute();
+            MessageBox.Show("Data written to Google Spreadsheet successfully!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class GoogleSheetsAuthentication
+    {
+        static string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        static string ApplicationName = "ABC";
+        static string ClientSecretFilePath;
+
+        static GoogleSheetsAuthentication()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            ClientSecretFilePath = Path.Combine(currentDirectory, "client_secret_529940815053-9c2s5kpb0tm5lgd7lhlmramlj1eb5rhe.apps.googleusercontent.com.json");
+        }
+
+        public static SheetsService GetService()
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream(ClientSecretFilePath, FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/sheets-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets, Scopes, "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets service using the authorized credential
+            return new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+    }
+}
